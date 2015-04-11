47e605870440fdb60bc53535757e264890243dab
MohamedMarwan <mohamed_marwan_m@hotmail.com>
2015-04-11 05:50:56 +0200

    another adProposal file added.

--- routes/adProposal2.0.js
+++ routes/adProposal2.0.js
@@ -1,41 +1,39 @@
 var express       = require('express');
 var router        = express.Router();
-var bodyParser    = require('body-parser');
-var bodyParsedUrl = bodyParser.urlEncoded({extended: true});
+var bodyParsedUrl = require('body-parser').urlEncoded({extended: true});
 var UserInterest  = require('../models/index.js').UserInterest;
 var http          = require('http');
+var request       = require('request');
+var inspect       = require('eyespect').inspector();
 
 router.route('/')
 .post(bodyParsedUrl, function(request, response){
 	
 	var adSize 	     = request.body.adSize;
 	var offeredPrice = request.body.offeredPrice;
-	var dspRes       = {};
 	var data         = {};
 	
 	var options = {
 		host: 'localhost',
 		port: 4000,
+		body: data,
 		path: '/',
 		method: 'POST'
 	};
 	
 	UserInterest.find({where:{userId: request.body.userId}}).then(function(userInterests) {
 		if(!userInterests) return response.sendStatus(404);
-		request.on('finish', function(){			
-			data['size'] = adSize;
-			data['interests'] = userInterests;
-			data['offeredPrice'] = offeredPrice;
-			data['userId'] = userId;
-			var req = http.request(options, function(res) {				
-				res.on('data', function (chunk) {
-					dspRes = dspRes + chunk;
-				});
-			});
-			req.write(data);
-			req.end();
-		});
-		response.send(dspRes);
+		data['size'] = adSize;
+		data['interests'] = userInterests;
+		data['offeredPrice'] = offeredPrice;
+		data['userId'] = userId;
+		request(options, function(err, res, body) {
+			if(err) {
+				inspect(err, 'error posting JSON')
+				return;
+			}
+			response.send(res);
+		})
 	});
 });
 
